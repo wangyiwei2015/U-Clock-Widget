7d7479af13608ed35876ca99c74da62f2199f9e0
wangyiwei <wangyw.dev@outlook.com>
2022-03-30 18:16:44 +0800

    init

---
+++ uclockwidget/uclockwidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uclockwidget
+//
+//  Created by wyw on 2022/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uclockwidget/uclockwidget/uclockwidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  uclockwidgetApp.swift
+//  uclockwidget
+//
+//  Created by wyw on 2022/3/30.
+//
+
+import SwiftUI
+
+@main
+struct uclockwidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
